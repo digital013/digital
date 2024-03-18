6629e8ef414e0b5c705693a6868755cc31a9bf23
digital013 <156333960+digital013@users.noreply.github.com>
2024-03-18 14:49:46 +0530

    card preview changed

--- project-info/projectscript.js
+++ project-info/projectscript.js
@@ -1,9 +1,18 @@
 const slider = document.querySelector('.slider');
 
 function activate(e) {
-  const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+  const items = document.querySelectorAll('.item'); // Select all elements with class "item"
+  
+  // If the clicked element matches the class "next" or a card itself, move the first item to the end of the slider
+  if (e.target.matches('.next') || e.target.classList.contains('item')) {
+    slider.append(items[0]);
+  }
+  
+  // If the clicked element matches the class "prev", move the last item to the beginning of the slider
+  if (e.target.matches('.prev')) {
+    slider.prepend(items[items.length - 1]);
+  }
 }
 
-document.addEventListener('click',activate,false);
+// Add a click event listener to the document, calling the "activate" function when a click occurs
+document.addEventListener('click', activate, false);
